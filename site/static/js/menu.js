1b8e42ac7d54ba936dabdd41783097e4ad37c412
solaraengineer <solaradeveloper@gmail.com>
2025-11-21 11:43:13 +0100

    Inital for Intro at 21.11.2025

--- site/static/js/menu.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("django").addEventListener("click", () => {
-    window.location.href = "../../django.html";
-});
-
-document.getElementById("javascript").addEventListener("click", () => {
-    window.location.href = "../../javascript.html";
-});
